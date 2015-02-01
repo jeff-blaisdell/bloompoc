4755a317c80f3f65e543e23ba59c161e50a83b3f
islandworksmn <islandworksmn@gmail.com>
2015-02-01 11:44:37 -0600

    Adding in code to read the 271 response from the rest service instead of a flat file.

--- bloompoc/src/main/java/com/bloom/app/poc/App271.java
+++ bloompoc/src/main/java/com/bloom/app/poc/App271.java
@@ -2,96 +2,135 @@ package com.bloom.app.poc;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
 
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.http.client.ClientProtocolException;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
 import org.milyn.Smooks;
 import org.milyn.SmooksException;
 import org.milyn.container.ExecutionContext;
 import org.milyn.event.report.HtmlReportGenerator;
-import org.milyn.io.StreamUtils;
 import org.xml.sax.SAXException;
 
 import com.bloom.app.poc.model271.Response271;
 
+public class App271 {
+	
+	private byte[] messageIn = readInputMessage();
 
-public class App271 
-{
-	private static byte[] messageIn = readInputMessage();
+	private final Smooks smooks;
 
-    private final Smooks smooks;
+	protected App271() throws IOException, SAXException {
+		// Instantiate Smooks with the config...
+		smooks = new Smooks("smooks-271-config.xml");
+	}
 
-    protected App271() throws IOException, SAXException {
-        // Instantiate Smooks with the config...
-        smooks = new Smooks("smooks-271-config.xml");
-    }
+	protected org.milyn.payload.JavaResult runSmooksTransform(
+			ExecutionContext executionContext) throws IOException,
+			SAXException, SmooksException {
+		try {
+			Locale defaultLocale = Locale.getDefault();
+			Locale.setDefault(new Locale("en", "IE"));
 
-    protected org.milyn.payload.JavaResult runSmooksTransform(ExecutionContext executionContext) throws IOException, SAXException, SmooksException {
-    	try {
-            Locale defaultLocale = Locale.getDefault();
-            Locale.setDefault(new Locale("en", "IE"));
+			org.milyn.payload.JavaResult javaResult = new org.milyn.payload.JavaResult();
 
-            org.milyn.payload.JavaResult javaResult = new org.milyn.payload.JavaResult();
+			// Configure the execution context to generate a report...
+			executionContext.setEventListener(new HtmlReportGenerator(
+					"target/report/report271.html"));
 
-            // Configure the execution context to generate a report...
-            executionContext.setEventListener(new HtmlReportGenerator("target/report/report271.html"));
+			// Filter the input message to the outputWriter, using the execution
+			// context...
+			smooks.filterSource(executionContext, new StreamSource(
+					new ByteArrayInputStream(messageIn)), javaResult);
 
-            // Filter the input message to the outputWriter, using the execution context...
-            smooks.filterSource(executionContext, new StreamSource(new ByteArrayInputStream(messageIn)), javaResult);
+			Locale.setDefault(defaultLocale);
 
-            Locale.setDefault(defaultLocale);
+			return javaResult;
+		} finally {
+			smooks.close();
+		}
+	}
 
-            return javaResult;
-        } finally {
-            smooks.close();
-        }
-    }
+	public static void main(String[] args) throws IOException, SAXException, SmooksException {
+		App271 app = new App271();
+		app.processResponse271();
+	}
+	
+	private void processResponse271() throws SmooksException, IOException, SAXException {
+		System.out.println("\n\n==============Message In==============");
+		System.out.println(new String(messageIn));
+		System.out.println("======================================\n");
 
-    public static void main(String[] args) throws IOException, SAXException, SmooksException {
-        System.out.println("\n\n==============Message In==============");
-        System.out.println(new String(messageIn));
-        System.out.println("======================================\n");
+		pause("The EDI input stream can be seen above.  Press 'enter' to see how this stream is transformed the Order Object graph...");
 
-        pause("The EDI input stream can be seen above.  Press 'enter' to see how this stream is transformed the Order Object graph...");
+		ExecutionContext executionContext = smooks.createExecutionContext();
+		org.milyn.payload.JavaResult result = runSmooksTransform(executionContext);
 
-        App271 app = new App271();
-        ExecutionContext executionContext = app.smooks.createExecutionContext();
-        org.milyn.payload.JavaResult result = app.runSmooksTransform(executionContext);
+		System.out.println("\n==============EDI as Java Object Graph=============");
+		Response271 response = (Response271) result.getBean("response271");
+		System.out.println(response);
+		System.out.println("======================================\n\n");
 
+		pause("And that's it!  Press 'enter' to finish...");
+		
+	}
 
-        System.out.println("\n==============EDI as Java Object Graph=============");
-        Response271 response = (Response271) result.getBean("response271");
-        System.out.println(response);
-        System.out.println("======================================\n\n");
+	private byte[] readInputMessage() {
+		String response = hitRestService();
+		return response.getBytes();
+	}
 
-        pause("And that's it!  Press 'enter' to finish...");
-    }
+	private void pause(String message) {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("> " + message);
+			in.readLine();
+		} catch (IOException e) {
+		}
+		System.out.println("\n");
+	}
 
-    private static byte[] readInputMessage() {
-        try {
-            return StreamUtils.readStream(new FileInputStream("271-input-message_small.edi"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "<no-message/>".getBytes();
-        }
-    }
+	public org.milyn.payload.JavaResult runSmooksTransform() throws IOException, SAXException {
+		ExecutionContext executionContext = smooks.createExecutionContext();
+		return runSmooksTransform(executionContext);
+	}
 
-    private static void pause(String message) {
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("> " + message);
-            in.readLine();
-        } catch (IOException e) {
-        }
-        System.out.println("\n");
-    }
+	private String hitRestService() {
 
-    public org.milyn.payload.JavaResult runSmooksTransform() throws IOException, SAXException {
-        ExecutionContext executionContext = smooks.createExecutionContext();
-        return runSmooksTransform(executionContext);
-    }
+		StringBuffer output = new StringBuffer();
+		try {
+
+			ClientRequest request = new ClientRequest(
+					"http://localhost:8080/bloom-0.0.1-SNAPSHOT/rest/eligibility/hello%20world");
+			request.accept("application/json");
+			ClientResponse<String> response = request.get(String.class);
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new ByteArrayInputStream(response.getEntity().getBytes())));
+
+			String line = null;
+			System.out.println("Output from Server .... \n");
+			while ((line = br.readLine()) != null) {
+				output.append(line + "\n");
+			}
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return output.toString();
+	}
 }
