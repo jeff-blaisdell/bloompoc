5ee642e2101fceddd39ca454b724104c439a1ad9
islandworksmn <islandworksmn@gmail.com>
2015-02-02 10:57:47 -0600

    Adding logic for two different return values.

--- test/bloom/src/main/java/com/bloom/MessageRestService.java
+++ test/bloom/src/main/java/com/bloom/MessageRestService.java
@@ -9,10 +9,10 @@ import javax.ws.rs.core.Response;
 public class MessageRestService { 
 	
 	@GET
-	@Path("/{param}")
-	public Response eligibility(@PathParam("param") String msg) {
+	@Path("{id}")
+	public Response eligibility(@PathParam("id") String id) {
  
-		String result = "ISA*00* *00* *01*091712414 *ZZ*TPNAME *030124*0900*U*00401*100000001*0*P*^~\n"
+		String resultA = "ISA*00* *00* *01*091712414 *ZZ*TPNAME *030124*0900*U*00401*100000001*0*P*^~\n"
 				+ "GS*HB*952931460*TPNAME*20030124*0900*1*X*004010X092~\n"
 				+ "ST*271*0001~\n"
 				+ "BHT*0022*11*100000001*20030124*0900~\n"
@@ -24,8 +24,23 @@ public class MessageRestService {
 				+ "IEA*1*100000001~\n"
 				;
 		
+		String resultB = "ISA*00* *00* *01*091712414 *ZZ*TPNAME *030124*0900*U*00401*100000001*0*P*^~\n"
+				+ "GS*HB*952931460*TPNAME*20030124*0900*1*X*004010X092~\n"
+				+ "ST*271*0001~\n"
+				+ "BHT*0022*11*100000001*20030124*0900~\n"
+				+ "HL*1**20*1~\n"
+				+ "NM1*PR*2*BLUECROSS BLUESHEILD*****FI*952931460~\n"
+				+ "PER*IC*EDI SUPPORT*TE*8002037729~\n"
+				+ "SE*42*0001~\n"
+				+ "GE*1*0001~\n"
+				+ "IEA*1*100000001~\n"
+				;
+		
+		if( "a".equals(id) ) {
+			return Response.status(200).entity(resultA).build();
+		}
  
-		return Response.status(200).entity(result).build();
+		return Response.status(200).entity(resultB).build();
  
 	} 
  
